b7cf72735170f8bb0ce4d93be08a9e98721a67dc
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-01-11 21:11:28 +0200

    main.py file was created

---
+++ main.py
@@ -0,0 +1,17 @@
+def add_full_name(user: dict) -> None:
+  # write your code here
+  user["full_name"] = f"{user['first_name']} {user['last_name']}"
+
+  print(user)
+
+# {
+#   "first_name": "Ivan",
+#   "last_name": "Vasylchenko",
+# }
+
+user = {
+  "first_name": "Ivan",
+  "last_name": "Vasylchenko",
+}
+
+add_full_name(user)
